8d6818ce1a53e035a3ef6e8432c62dcbc13114bf
ShellyHsueh <hsueh821203@gmail.com>
2018-05-21 23:36:53 +0800

    flash bar added

--- application/controllers/auth.rb
+++ application/controllers/auth.rb
@@ -1,6 +1,7 @@
 module TalkUp
   # Web App for login (authentication)
   class App < Roda
+    class UnauthorizedError < StandardError; end
 
     route('auth') do |routing|
       routing.on 'login' do
@@ -11,13 +12,16 @@ module TalkUp
 
         # POST /auth/login
         routing.post do
-          account =  ApiGateway.new.account_auth( routing.params['username'],
-                                                  routing.params['password'] )
-          session[:current_account] = account
-          # flash[:notice] = "Welcome to TalkUp, #{login_input['username']}!"
+          account_response =  ApiGateway.new.account_auth( routing.params['username'],
+                                                           routing.params['password'] )
+          raise(UnauthorizedError) unless account_response.code == 200
+
+          session[:current_account] = account_response
+          current_username = JSON.parse(account_response.message)['username']
+          flash[:notice] = "Welcome to TalkUp, #{current_username}!"
           routing.redirect '/'
         rescue StandardError
-          # flash[:error] = 'Username and Password did not match our records'
+          flash[:error] = 'Username and Password did not match our records'
           routing.redirect '/auth/login'
         end
       end
